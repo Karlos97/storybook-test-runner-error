ca4d9fea4e7f63d391b0286cbe7b7fba5afda72c
Karlos97 <55185375+Karlos97@users.noreply.github.com>
2023-06-05 10:15:20 +0200

    Upload .storybook files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,1 +1,20 @@
-
+import type { StorybookConfig } from "@storybook/react-webpack5";
+const config: StorybookConfig = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-interactions",
+        "@storybook/addon-a11y", //👈 The a11y addon goes here
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    staticDirs: ["../public"],
+};
+export default config;
